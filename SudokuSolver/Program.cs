ee17332483bb970890f9b1b51aa4ab7d65605c6c
mcmcelro <mcmcelro@yahoo.com>
2018-10-15 10:32:22 -0400

    Add error handling and file reading.

--- SudokuSolver/Program.cs
+++ SudokuSolver/Program.cs
@@ -21,29 +21,85 @@ namespace SudokuSolver
         }
     }
 
+    // abstract class defining functions needed for board sections,
+    // which could be either the 3x3 sections or the whole board
     abstract class SudokuSection
     {
+        // initial values assigned to section
+        internal string initialValues;
+
+        // is this a valid solution?
         internal abstract bool isValid();
+
+        // does the section have child sections?
         internal abstract bool hasChildren();
+
+        // reset to initial values
+        internal abstract void resetToStart();
     }
 
+    // class for a 3x3 board section
     class SudokuSubsection : SudokuSection
     {
         int[] values;
 
+        // default constructor with no input
         public SudokuSubsection()
         {
-
+            initialValues = "XXXXXXXXX";
+            values = new int[9];
         }
 
+        // constructor with a 9-character string of values
+        public SudokuSubsection(string sectionValues)
+        {
+            initialValues = sectionValues;
+            resetToStart();
+        }
+        // constructor with a 9-character input string indicating starting values
+
+        // is this a valid solution? i.e., no repeats
         internal override bool isValid()
         {
             return values.Where(val => val != 0).Distinct().Count() == 9;
         }
 
+        // does the section have child sections? should always be false
         internal override bool hasChildren()
         {
             return false;
+        }
+
+        // reset to initial values
+        internal override void resetToStart()
+        {
+            values = new int[9];
+            if(initialValues.Length != 9)
+            {
+                throw new InvalidDataException($"Invalid tile size {initialValues.Length} detected.");
+            }
+            try
+            {
+                for (int i = 0; i < 9; i++)
+                {
+                    if (initialValues[i] == 'X')
+                    {
+                        values[i] = 0;
+                    }
+                    else if (initialValues[i] > '9' || initialValues[i] < '1')
+                    {
+                        throw new InvalidDataException($"Invalid input character {initialValues[i]} found in board.");
+                    }
+                    else
+                    {
+                        values[i] = initialValues[i] - '0';
+                    }
+                }
+            }
+            catch (IndexOutOfRangeException ex)
+            {
+                throw new InvalidDataException("Invalid number of values found in a tile.");
+            }
         }
     }
 
@@ -53,6 +109,7 @@ namespace SudokuSolver
 
         public SudokuBoard()
         {
+            tiles = new List<SudokuSection>();
             for (int i = 0; i < 9; i++)
             {
                 tiles.Add(new SudokuSubsection());
@@ -63,11 +120,16 @@ namespace SudokuSolver
         {
             try
             {
-                string boardData = new StreamReader(File.Open(boardFile, FileMode.Open)).ReadToEnd();
+                initialValues = new StreamReader(File.OpenRead(boardFile)).ReadToEnd();
+                resetToStart();
             }
             catch (IOException ex)
             {
-                Console.Error.WriteLine("Could not open board file for input.");
+                Console.Error.WriteLine($"Could not open board file for input. Details: {ex.Message}");
+            }
+            catch (InvalidDataException ex)
+            {
+                Console.Error.WriteLine(ex.Message);
             }
         }
 
@@ -84,6 +146,23 @@ namespace SudokuSolver
         internal override bool hasChildren()
         {
             return true;
+        }
+
+        internal override void resetToStart()
+        {
+            tiles = new List<SudokuSection>();
+            using (StringReader valueLines = new StringReader(initialValues))
+            {
+                string line;
+                while ((line = valueLines.ReadLine()) != null)
+                {
+                    tiles.Add(new SudokuSubsection(line));
+                }
+            }
+            if (tiles.Count() > 9 || tiles.Count() < 1)
+            {
+                throw new InvalidDataException("Invalid board input detected.");
+            }
         }
     }
 }
