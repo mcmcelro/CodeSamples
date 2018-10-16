3a4b592b3780aac5cdd8178de2d0d082d090d192
Michael McElroy <mcmcelro@yahoo.com>
2018-10-16 10:03:41 -0400

    Finalized C# version
    Refactored to use jagged arrays so Linq querying works (instead of nested 'for' loops); removed need for passing the board to each function; finished solving function.

--- SudokuSolver/Program.cs
+++ SudokuSolver/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.IO;
+using System.Linq;
 using System.Reflection;
 
 namespace SudokuSolver
@@ -18,88 +19,83 @@ namespace SudokuSolver
             {
                 SudokuBoard boardToSolve = new SudokuBoard(args[0]);
                 boardToSolve.solveBoard();
+                Console.WriteLine("Solving complete.");
             }
             catch (Exception ex)
             {
                 Console.Error.WriteLine(ex.Message);
-                Console.ReadLine();
             }
         }
     }
 
     class SudokuBoard
     {
-        int[,] values;
+        int[][] values;
         string outputFileName;
 
         public SudokuBoard(string boardFile)
         {
-            values = new int[9, 9];
+            values = new int[9][];
             outputFileName = Path.GetFileNameWithoutExtension(boardFile) + ".sol.txt";
             string initialValues = new StreamReader(File.OpenRead(boardFile)).ReadToEnd().Replace("\n", "").Replace("\r", "");
+            if (initialValues.Length != 81) throw new InvalidDataException($"Invalid board size: {initialValues.Length} (should be 81)");
 
             for (var row = 0; row < 9; row++)
             {
+                values[row] = new int[9];
                 for (var column = 0; column < 9; column++)
                 {
                     char thisVal = initialValues[9 * row + column];
-                    if ((thisVal < '0' || thisVal > '9') && thisVal != 'X')
+                    if ("123456789X".IndexOf(thisVal) < 0)
                         throw new InvalidDataException($"Invalid character found on board: {thisVal}");
-                    values[row, column] = thisVal == 'X' ? 0 : thisVal - '0';
+                    values[row][column] = thisVal == 'X' ? 0 : thisVal - '0';
                 }
             }
-            if (initialValues.Length != 81) throw new InvalidDataException($"Invalid board size: {initialValues.Length} (should be 81)");
         }
 
         // see if a row contains a number
-        private bool rowContainsNumber(int[,] passedBoard, int row, int number)
+        private bool rowContainsNumber(int row, int number)
         {
-            for (int column = 0; column < 9; column++)
-                if (passedBoard[row, column] == number) return true;
-
-            return false;
+            return values[row].Contains(number);
         }
 
         // see if a column contains a number
-        private bool columnContainsNumber(int[,] passedBoard, int column, int number)
+        private bool columnContainsNumber(int column, int number)
         {
-            for (int row = 0; row < 9; row++)
-                if (passedBoard[row, column] == number) return true;
-
-            return false;
+            return values.Select(row => row[column]).Contains(number);
         }
 
-        // see if <number> is used in a 3x3 section starting at [sectionStartingRow, sectionStartingColumn]
-        private bool sectionContainsNumber(int[,] passedBoard, int sectionStartingRow, int sectionStartingColumn, int number)
+        // see if <number> is used in a 3x3 section starting at [sectionStartingRow][sectionStartingColumn]
+        private bool sectionContainsNumber(int sectionStartingRow, int sectionStartingColumn, int number)
         {
             for (int row = sectionStartingRow; row < sectionStartingRow + 3; row++)
             {
                 for (int column = sectionStartingColumn; column < sectionStartingColumn + 3; column++)
                 {
-                    if (passedBoard[row, column] == number) return true;
+                    if (values[row][column] == number) return true;
                 }
             }
             return false;
         }
 
-        // see if [row, column] can legally hold <number>
-        private bool canSpaceHoldNumber(int[,] passedBoard, int row, int column, int number)
+        // see if [row][column] can legally hold <number>
+        private bool canSpaceHoldNumber(int row, int column, int number)
         {
             // if the row does not contain the number...
-            return !rowContainsNumber(passedBoard, row, number)
+            return !rowContainsNumber(row, number)
             // and the column does not contain the number...
-            && !columnContainsNumber(passedBoard, column, number)
+            && !columnContainsNumber(column, number)
             // and the 3x3 section that the square falls in does not contain the number...
-            && !sectionContainsNumber(passedBoard, row - row % 3, column - column % 3, number);
+            && !sectionContainsNumber(row - row % 3, column - column % 3, number);
             // you can put the number in this space!
         }
 
         // find the first empty space; returns (row: -1, column: -1) if none are found
-        private (int row, int column) getEmptySpace(int[,] passedBoard)
+        private (int row, int column) getEmptySpace(int[][] values)
         {
             for (int row = 0; row < 9; row++)
                 for (int column = 0; column < 9; column++)
-                    if (passedBoard[row, column] == 0) return (row: row, column: column);
+                    if (values[row][column] == 0) return (row: row, column: column);
 
             return (-1, -1);
         }
@@ -115,7 +111,7 @@ namespace SudokuSolver
                     {
                         for (int column = 0; column < 9; column++)
                         {
-                            outputFile.Write(values[row, column] + "");
+                            outputFile.Write(values[row][column] + "");
                         }
                         outputFile.Write("\r\n");
                     }
@@ -127,17 +123,28 @@ namespace SudokuSolver
             }
         }
 
-        private bool solve(int[,] passedBoard)
+        private bool solve(int[][] values)
         {
             // methodology:
             // 1. find the first empty square on the board
             // 2. loop through numbers 1-9 to see which one can be placed there
             // 3. if a number can be placed, place it and pass the new board to a
             //    recursive call to the solver function
-            // 4. if a number can't be placed, wipe the number back out and return to the last
-            //    recursive call to the solver function, proceeding to the next potential solution
+            // 4. if a number can't be placed, wipe the number back out and step back to the
+            //    parent call, proceeding to the next potential solution 1-9
             // 5. if we get all the way through 9 on the first empty space and can't place a number,
             //    it's an invalid board and there is no solution
+            var firstEmptySpace = getEmptySpace(values);
+            if (firstEmptySpace.row == -1 && firstEmptySpace.column == -1) return true;
+            for (int valToTest = 1; valToTest <= 9; valToTest++)
+            {
+                if (canSpaceHoldNumber(firstEmptySpace.row, firstEmptySpace.column, valToTest))
+                {
+                    values[firstEmptySpace.row][firstEmptySpace.column] = valToTest;
+                    if (solve(values)) return true;
+                    values[firstEmptySpace.row][firstEmptySpace.column] = 0;
+                }
+            }
             return false;
         }
     }
